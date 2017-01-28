c4715debff1cec0ea04067f14365e0d5b0d9f0fd
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-01-28 11:25:49 +0100

    Create DataEventType

---
+++ Alert/DataEventType.swift
@@ -0,0 +1,15 @@
+//
+//  DataEventType.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 24/01/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+enum DataEventType {
+    case added
+    case updated
+    case removed
+}
