6f0f5f211f653e8980719aa8ece7b27d0eefc65e
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-01 20:53:54 +0100

    Create MutableAlert

---
+++ Alert/MutableAlert.swift
@@ -0,0 +1,16 @@
+//
+//  MutableAlert.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 01/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct MutableAlert {
+    var description: String?
+    var matterName: String?
+    var creationDate: RecordDate?
+    var triggerDate: RecordDate?
+}
