8dddfee951af1b8d000c634d6f4036a9cc87eab4
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-01 20:52:36 +0100

    Create ServerValue

---
+++ Alert/ServerValue.swift
@@ -0,0 +1,13 @@
+//
+//  ServerValue.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 01/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+protocol ServerValue {
+    static var currentTime: Any { get }
+}
