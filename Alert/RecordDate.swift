feed8c70771a93e20391e08f3d78b1d45306125c
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-01 20:54:45 +0100

    Create RecordDate

---
+++ Alert/RecordDate.swift
@@ -0,0 +1,14 @@
+//
+//  RecordDate.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 01/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+enum RecordDate {
+    case server
+    case localDate(date: Date)
+}
