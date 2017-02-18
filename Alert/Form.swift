8fa6f41a62aeae51e3863692e430064510b05e61
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-18 15:01:57 +0100

    Create Form

---
+++ Alert/Form.swift
@@ -0,0 +1,14 @@
+//
+//  Form.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 18/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct Form {
+    var id: String
+    var name: String
+}
