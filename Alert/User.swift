ccd3aac420f924857456def26378dbc68f00f691
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-18 11:14:06 +0100

    Create User

---
+++ Alert/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 12/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    let id: String
+    let email: String
+    let name: String
+}
