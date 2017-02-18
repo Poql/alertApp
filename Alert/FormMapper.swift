98cb8e57143f8710eaf2134d4996562f210bcb7d
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-02-18 15:02:45 +0100

    Create FormMapper

---
+++ Alert/FormMapper.swift
@@ -0,0 +1,17 @@
+//
+//  FormMapper.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 18/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct FormMapper {
+    static func form(from snapshot: Snapshot) -> Form? {
+        guard let name = snapshot.dictionary["name"] as? String else { return nil }
+        return Form(id: snapshot.id, name: name)
+    }
+}
+
