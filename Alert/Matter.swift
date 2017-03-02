be03a5f73504d8cb0c2a050f9cde68c339c5ea83
Gaétan Zanella <gaetan.zanella@gmail.com>
2017-03-02 20:52:58 +0100

    Create Matter

---
+++ Alert/Matter.swift
@@ -0,0 +1,15 @@
+//
+//  Matter.swift
+//  Alert
+//
+//  Created by Gaétan Zanella on 25/02/2017.
+//  Copyright © 2017 Gaétan Zanella. All rights reserved.
+//
+
+import Foundation
+
+struct Matter {
+    let id: String
+    let name: String
+}
+
